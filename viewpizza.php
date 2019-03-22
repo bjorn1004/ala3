700a66a036ea22a2efc465da7998a0b7131c2ffb
DylanTrueOtaku <dylan8.riet@gmail.com>
2019-03-22 10:39:58 +0100

    Vieuw pizza changes

--- viewpizza.php
+++ viewpizza.php
@@ -1,5 +1,9 @@
-<?php include 'header.php';
-    include 'drawcard.php';
+<?php include 'header.php';?>
+<?php include 'drawcard.php';
 drawCard($_GET['p_ID'])?>
+
 <!--moet een ontwerp voor deze pagina hebben-->
+<div id="chart">
+  <img src="img/<?= $pizza['name']?>.png" class="card-img-top" alt="pizzapic">
+</div>
 <?php include 'footer.php';?>
