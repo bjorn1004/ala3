0bb3af2892e091fd53fa3640de48ce66c1b20360
bjorn1004 <bjorn1004@gmail.com>
2019-03-29 10:34:39 +0100

    Favicon toegevoegd

--- header.php
+++ header.php
@@ -2,25 +2,43 @@
 <html>
 
 <head>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="style.css">
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width = device-width, initial-scale = 1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title> Sopranos Pizza </title>
+    <link rel="apple-touch-icon" sizes="57x57" href="favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width = device-width, initial-scale = 1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Sopranos Pizza </title>
 </head>
 
 <body>
-  <header>
-    <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <h1 class="display-4">Sopranos Pizza</h1>
-        <p class="lead">Pizza bereidt op een traditioneel Italiaanse manier</p>
-      </div>
-    </div>
-    <nav class="nav">
-      <a class="nav-link active" href="index.php">Home</a>
-      <a class="nav-link" href="about.php">About</a>
-      <a class="nav-link" href="locaties.php">Locaties</a>
-    </nav>
-  </header>
+    <header>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+               <img src="img/pizzalogo.png" alt="Pizza logo">
+                <h1 class="display-4">Sopranos Pizza</h1>
+                <p class="lead">Pizza bereidt op een traditioneel Italiaanse manier</p>
+            </div>
+        </div>
+        <nav class="nav">
+            <a class="nav-link active" href="index.php">Home</a>
+            <a class="nav-link" href="about.php">About</a>
+            <a class="nav-link" href="locaties.php">Locaties</a>
+        </nav>
+    </header>
